bf0599d42fd6be3f4fc13579a422afd9864cee80
中川拓哉 <aboutmore7@gmail.com>
2023-12-24 07:59:53 +0000

    fix: base app

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ RUN <<EOF
   npm install -g @angular/cli
   apt-get update
   apt-get install git
+  npm install
 EOF
 EXPOSE 4200
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ForecastComponent } from './forecast/forecast.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ForecastComponent,
+    title: '洗車予報',
+  },
+];
