b1dbf93be45d8d713a7e24597544b7b56fd12e49
中川拓哉 <aboutmore7@gmail.com>
2023-12-25 23:11:46 +0000

    fix: add Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { ForecastComponent } from './forecast/forecast.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
+    title: 'My Angular App',
+  },
+  {
+    path: 'forecast',
     component: ForecastComponent,
-    title: '洗車予報',
+    title: '天気予報',
   },
 ];
